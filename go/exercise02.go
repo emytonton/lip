5ca35ea30de169670df6587a1b939a67b318f614
Emilly Paiva <emillypaiva3260@gmail.com>
2025-06-13 14:17:24 -0300

    build: exercise 2 go

---
+++ go/exercise02.go
@@ -0,0 +1,21 @@
+package lasagna
+
+
+const OvenTime = 40
+
+
+func RemainingOvenTime(actualMinutesInOven int) int {
+	return OvenTime - actualMinutesInOven
+}
+
+
+
+func PreparationTime(numberOfLayers int) int {
+	return numberOfLayers * 2
+}
+
+
+func ElapsedTime(numberOfLayers, actualMinutesInOven int) int {
+	return PreparationTime(numberOfLayers) + actualMinutesInOven
+}
+
