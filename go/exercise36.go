f695107f6fe256bc5c985a34ae8fa8ca25eba14d
Emilly Paiva <emillypaiva3260@gmail.com>
2025-06-15 14:45:37 -0300

    build: exercise 36 go

---
+++ go/exercise36.go
@@ -0,0 +1,24 @@
+package isogram
+
+import (
+	"strings"
+	"unicode"
+)
+
+func IsIsogram(word string) bool {
+	seen := make(map[rune]bool)
+	
+	for _, char := range strings.ToLower(word) {
+		if char == ' ' || char == '-' {
+			continue
+		}
+		if !unicode.IsLetter(char) {
+			continue
+		}
+		if seen[char] {
+			return false
+		}
+		seen[char] = true
+	}
+	return true
+}
